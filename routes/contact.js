077c475413c4adfc1a1dbbe1215fc921cf873e23
Amina CHABCHOUB <chabchoub.amina@esprit.tn>
2022-04-02 22:57:14 +0100

    update et recherche par nom

--- routes/contact.js
+++ routes/contact.js
@@ -23,22 +23,21 @@ router.get('/contact2', function(req, res, next) {
 
    
 //find by id
-router.get('/getbyid/:id',function(req, res , next) {
-    const ident = (req.params.id).trim() ; 
-    Contact.findById(ident).then(data=>{
-        if(!data){
-            res.status(404).send({message : "Not found contact with id" + ident}) ; 
-        }else
-        res.send(data)
-    }); 
+//post kkhater bech nati des valures f forualire
+router.post('/search',function(req, res , next) {
+   Contact.find({FullName : req.body.search , Phone : req.body.pn}, function(err , data){
+       if (err) throw err ; 
+       res.render("contact.twig", {data}) ; 
+   }); 
    });
 
 // ajouter
+//ouvrir le fichier 
 router.get('/addform', function(req, res, next) {
     
         res.render('add.twig') ; 
 });
-
+//action insert data eli t3aytelha f formulaire
 router.post('/addaction' , function(req, res, next){
     var c = new Contact({
         FullName :  req.body.FullName , 
@@ -49,43 +48,25 @@ router.post('/addaction' , function(req, res, next){
     res.redirect("/contact/contact2")
 })
 
+
 //update
-router.get('/update',function(req, res , next) {
-    res.render('update.twig') ; 
-   });
-router.put('/updateaction/:id', function(req, res, next) {
-    var c = req.body ; 
-    var ident = (req.params.id).trim() ; 
-    Contact.findByIdAndUpdate(ident, c , {useFindAndModify:false}).then(data=>{
-        res.send(data) ; 
+router.get('/update/:id',function(req, res , next) {
+    var ident = req.params.id ; 
+    Contact.findById({_id : ident} , function(err , doc){
+    if(err) throw err ; 
+    res.render('update.twig' , {doc}) ; 
     })
-  
-});
-
-//update
-router.get('/update',function(req, res , next) {
-   res.render('update.twig') ; 
   });
+router.post('/updateaction' , function(req,res,next){
+    var ident = req.body.id ; 
+    Contact.findById({_id : ident} , function(err , doc){
+        doc.FullName = req.body.FullName ; 
+        doc.Phone = req.body.Phone 
+        doc.save() ; 
+    }) ; 
+    res.redirect('/contact/contact2') ; 
+})
 
-router.put('/updateaction', function(req, res){
-    res.send('here') ; 
-    /*
-    
-    var collection = db.get('contacts');
-    var FullName = req.body.FullName ; 
-    console.log(FullName) ; 
-    var item = {
-        FullName: req.body.FullName,
-        Phone: req.body.Phone
-    };
-    var id = req.params.id;
-    collection.update(
-    {_id: id}, {
-        $set: item
-    });
-    res.redirect('/contact/contact2');
-    */
-});
 
 
 //delete
